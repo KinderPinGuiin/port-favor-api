a591fe22f5dd40673ca9fe6fd6820a24bc8006d2
Jordan <83838658+KinderPinGuiin@users.noreply.github.com>
2023-11-07 20:36:29 +0100

    fix: DTO serialization and build

--- src/main/java/fr/univrouen/portfavor/dto/request/authentication/AuthenticationRequestDTO.java
+++ src/main/java/fr/univrouen/portfavor/dto/request/authentication/AuthenticationRequestDTO.java
@@ -1,24 +1,18 @@
 package fr.univrouen.portfavor.dto.request.authentication;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.extern.jackson.Jacksonized;
 
 /**
  * Represents a login request.
+ *
+ * @param login    User login (username).
+ * @param password User password.
  */
-@RequiredArgsConstructor
-@Accessors(fluent = true) @Getter
-public class AuthenticationRequestDTO {
-
-    /**
-     * User login (username).
-     */
-    private final String login;
-
-    /**
-     * User password.
-     */
-    private final String password;
-
-}
+public record AuthenticationRequestDTO(
+    String login,
+    String password
+) {}
--- src/test/java/fr/univrouen/portfavor/PortfavorApplicationTests.java
+++ src/test/java/fr/univrouen/portfavor/PortfavorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PortfavorApplicationTests {
+class PortFavorApplicationTests {
 
 	@Test
 	void contextLoads() {
