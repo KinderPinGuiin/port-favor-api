e4f5d43357f65f38626c1a9764914df8accec0ed
Jordan <83838658+KinderPinGuiin@users.noreply.github.com>
2023-11-09 22:56:58 +0100

    fix: Removed args from TokenAuthenticationFilter constructor

--- src/main/java/fr/univrouen/portfavor/config/security/SecurityConfiguration.java
+++ src/main/java/fr/univrouen/portfavor/config/security/SecurityConfiguration.java
@@ -95,7 +95,7 @@ public class SecurityConfiguration {
         });
 
         // Add the TokenAuthenticationFilter to the security filter list
-        http.addFilter(new TokenAuthenticationFilter(this.authenticationManager(), this.authenticationService));
+        http.addFilter(new TokenAuthenticationFilter(this.authenticationManager()));
 
         return http.build();
     }
